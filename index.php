174fb9c7a561b11608be609e2ed6df12a80dd82f
Shaun <scalagos0123@live.com>
2017-04-05 14:19:22 +0800

    initial connection to database

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello World";
+$connect = new mysqli("52.187.113.8", "root", "feutech", "feutechdemo");
+$result = $connect->query("select * from users");
+
+?>
+
+<html>
+    <body>
+        <p>Hello World 2</p>
+    </body>
+</html>
